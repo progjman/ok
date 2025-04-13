17947364c908bc537fea058c7b690668897d0270
progjman <progjman@gmail.com>
2025-04-13 18:09:53 +0500

    Добавлено поле пароль

--- main.go
+++ main.go
@@ -10,8 +10,15 @@ import (
 
 func main() {
 	db.InitDB()
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
 	http.HandleFunc("/", handler.ShowRegisterPage)
 	http.HandleFunc("/check-username", handler.CheckUsername)
+
+	http.HandleFunc("/register/password-step", handler.RegisterPassword)
+	http.HandleFunc("/check-password", handler.CheckPassword)
+
 	log.Println("Сервер на http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
